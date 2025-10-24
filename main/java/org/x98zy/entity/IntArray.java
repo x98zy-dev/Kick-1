a7a1bbfa2882fee98a0b905ab7597e4c73f04aab
Artem <whaleswannalive@gmail.com>
2025-10-24 18:58:17 +0300

    .

---
+++ main/java/org/x98zy/entity/IntArray.java
@@ -0,0 +1,34 @@
+package org.x98zy.entity;
+
+public class IntArray {
+
+    private final int[] array;
+
+    public IntArray(int[] array) {
+        if(array == null) {
+            throw new IllegalArgumentException("Массив не может быть null");
+        }
+        this.array = array.clone();
+    }
+
+    public int[] getArray() {
+        return array.clone();
+    }
+
+    public int length() {
+        return array.length;
+    }
+
+    @Override
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i]);
+            if(i < array.length - 1)
+                sb.append(", ");
+        }
+        return sb.toString();
+    }
+}
---
+++ main/java/org/x98zy/factory/IntArrayFactory.java
@@ -0,0 +1,25 @@
+package org.x98zy.factory;
+
+import org.x98zy.entity.IntArray;
+
+public class IntArrayFactory {
+
+    public static final String EMPTY_ARRAY_MESSAGE = "Создан пустой массив";
+
+    private IntArrayFactory() {
+
+    }
+
+    public static IntArray createEmpty() {
+        return new IntArray(new int[0]);
+    }
+
+    public static IntArray createFromArray(int[] array) {
+        if (array != null) {
+            return new IntArray(array);
+        }
+        else {
+           return createEmpty();
+        }
+    }
+}
